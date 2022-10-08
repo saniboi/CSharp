c7677ff04d253472e9ecce25d1720eb81e475bd3
Kerim San <san.kerim@outlook.com>
2022-10-08 22:39:41 +0200

    git created

---
+++ 01_HelloWorld_normal/Program.cs
@@ -0,0 +1,11 @@
+﻿namespace _01_HelloWorld_normal
+{
+    class Program
+    {
+        //static void Main(string[] args)
+        static void Main()
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
